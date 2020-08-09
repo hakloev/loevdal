c7e322667ad251f19fd64d7f712282a4c851d454
Håkon Ødegård Løvdal <hakon.odegard.lovdal@schibsted.com>
2020-08-09 21:45:55 +0200

    build test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import hello from './hello.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Here will be dragons or something like that.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={hello} className="App-logo" alt="logo" />
       </header>
     </div>
   );
